8c53160aa04956828d92977d81902e262fccbad9
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2020-12-07 01:43:38 +0700

    ADD use environment variables for database connection

--- db/connect.php
+++ db/connect.php
@@ -1,12 +1,14 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "portfolio";
+    # Get credentials from environment variables
+    $host = getenv("DB_HOST");
+    $user = getenv("DB_USER");
+    $password = getenv("DB_PASSWORD");
+    $dbname = getenv("DB_NAME");
 
-    $con = mysqli_connect($servername, $username, $password, $dbname);
+    $con = mysqli_connect($host, $user, $password, $dbname);
 
-    if (!$con) {
-        die("Error in database : ".mysqli_error($con));  
-    }
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        exit();
+    } 
 ?>
