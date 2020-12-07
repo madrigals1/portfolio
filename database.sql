dae4d41af275325a58d6fe71d8055245aa57df9f
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2020-12-07 16:13:54 +0700

    FIX less comments in test database dump

--- database.sql
+++ database.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Хост: 127.0.0.1:3306
--- Время создания: Ноя 29 2019 г., 21:36
--- Версия сервера: 10.3.13-MariaDB-log
--- Версия PHP: 7.1.32
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -18,17 +9,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- База данных: `portfolio`
---
 CREATE DATABASE IF NOT EXISTS `portfolio` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `portfolio`;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `projects`
---
+-- Projects table
 
 CREATE TABLE `projects` (
   `id` int(11) NOT NULL,
@@ -45,19 +29,12 @@ CREATE TABLE `projects` (
   `small_pic` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Дамп данных таблицы `projects`
---
-
 INSERT INTO `projects` (`id`, `name`, `alias`, `short_desc`, `long_desc`, `date`, `lnf`, `play_link`, `github_link`, `visit_link`, `big_pic`, `small_pic`) VALUES
 (1, 'Testing Project', 'TestingAlias', 'Testing short_desc', 'Testing long_desc', 'April 2019', 'Test LNF', 'Test Play Link', 'Test Github Link', 'Test Visit Link', '1575052073-big.png', '1575052073-small.png'),
 (2, 'Testing Project 2', 'asdasd', 'asdasd', 'asdasd', 'asdasd', 'asdads', 'asdasd', 'asdasd', 'asdasd', '1575052150-big.png', '1575052150-small.png');
 
--- --------------------------------------------------------
 
---
--- Структура таблицы `users`
---
+-- Users table
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -65,18 +42,10 @@ CREATE TABLE `users` (
   `password` varchar(512) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Дамп данных таблицы `users`
---
-
 INSERT INTO `users` (`id`, `login`, `password`) VALUES
 (1, 'admin', '5583413443164b56500def9a533c7c70');
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `works`
---
+-- Works table
 
 CREATE TABLE `works` (
   `id` int(11) NOT NULL,
@@ -87,55 +56,29 @@ CREATE TABLE `works` (
   `picture` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Дамп данных таблицы `works`
---
-
 INSERT INTO `works` (`id`, `name`, `period`, `description`, `queue`, `picture`) VALUES
 (1, 'Test Journey', 'APR 2018 - JUN 2019', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 1, '1575052217-work.png'),
 (2, 'Test Journey 2', 'JUN 2019 - AUG 2019', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 2, '1575052395-work.png');
 
---
--- Индексы сохранённых таблиц
---
+-- Indexes
 
---
--- Индексы таблицы `projects`
---
 ALTER TABLE `projects`
   ADD PRIMARY KEY (`id`);
 
---
--- Индексы таблицы `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
---
--- Индексы таблицы `works`
---
 ALTER TABLE `works`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT для сохранённых таблиц
---
+-- AUTO_INCREMENT
 
---
--- AUTO_INCREMENT для таблицы `projects`
---
 ALTER TABLE `projects`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT для таблицы `users`
---
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT для таблицы `works`
---
 ALTER TABLE `works`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
