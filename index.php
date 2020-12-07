9b5843237db7956e833960e36e1d0762ba649c06
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2020-12-07 17:44:18 +0700

    ADD slash path before vendor imports

--- index.php
+++ index.php
@@ -15,10 +15,10 @@
   <title>Adi S. Portfolio</title>
 
   <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom fonts for this template -->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
   <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet'
@@ -26,7 +26,7 @@
   <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
 
   <!-- Custom styles for this template -->
-  <link href="vendor/css/agency.min.css" rel="stylesheet">
+  <link href="/vendor/css/agency.min.css" rel="stylesheet">
 
 </head>
 
